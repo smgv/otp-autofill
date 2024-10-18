60b248a194be685997d0bc5203075fa55ca19e4b
smgv <gpatra1996@gmail.com>
2024-10-18 13:30:41 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,49 @@
-import Image from "next/image";
+"use client";
+
+// import { Badge, Checkbox } from "@toruslabs/react-components";
+// import { Toggle } from "@toruslabs/react-components";
+// import { cn } from "@toruslabs/react-components";
+import { OtpInput } from "@toruslabs/react-components";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // const classVal = "text-teal-800";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [otp, setOtp] = useState("");
+
+  const handleOtpComplete = (val: string) => {
+    setOtp(val);
+  };
+  return (
+    <div className="flex flex-col gap-y-2 items-center justify-center h-screen">
+      {/* <p>Hello World</p>
+      <Button variant="primary">Click Me</Button>
+      <Button variant="secondary">Click Me</Button>
+      <Button variant="tertiary">Click Me</Button>
+      <Button variant="text">Click Me</Button>
+      <Button variant="primary" loading>
+        Click Me
+      </Button>
+      <Button variant="primary" disabled>
+        Click Me
+      </Button>
+      <Toggle initialState={true} />
+      <p className={cn("text-purple-400", classVal)}>hello</p>
+      <Tooltip
+        text="Hello, Hi"
+        placement="left"
+        classes={{ tooltipContainer: "bg-teal-400 p-4 rounded-md" }}
+      >
+        Hi Tooltip
+      </Tooltip> */}
+      <p>Your verification code is 123456.</p>
+      <p>@www.example.com #123456</p>
+      <br />
+      <p>Enter OTP</p>
+      <OtpInput length={6} onComplete={handleOtpComplete} />
+      <p>OTP: {otp || "- - - - - -"}</p>
+      {/* <Checkbox id="1" />
+      <Badge pill>Hello</Badge> */}
     </div>
   );
 }

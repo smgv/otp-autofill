3641556c527258eccc81081fb21988cf6940ae05
smgv <gpatra1996@gmail.com>
2024-10-22 15:31:25 +0530

    layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       >
         {children}
         <footer className="text-gray-500 underline mt-auto text-center mb-4">
-          v1.0.7
+          v1.0.8
         </footer>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,27 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { OtpInput } from "@/components/OtpInput";
+import { useState } from "react";
 
 export default function Home() {
-  const router = useRouter();
+  const [otp, setOtp] = useState("");
+
+  const handleOtpComplete = (val: string) => {
+    setOtp(val);
+  };
+
   return (
     <div className="flex flex-col gap-y-2 items-center justify-center h-full w-full">
-      <p>Example 1</p>
-      <form className="flex flex-col gap-y-1 my-4">
-        <label>Enter OTP:</label>
-        <input
-          type="text"
-          id="otp"
-          aria-label="otp"
-          name="otp"
-          autoComplete="one-time-code"
-          inputMode="numeric"
-          maxLength={6}
-          pattern="\d{6}"
-          className="border border-gray-500 px-4 py-2 rounded-md"
-          placeholder="Enter OTP"
-        />
-      </form>
-
-      <button
-        onClick={() => router.push("/verify")}
-        className="text-blue-500 hover:underline"
-      >
-        Go to WebOTP API Example
-      </button>
+      <code>
+        <p>Message Format:</p>
+        <br />
+        <p>Your verification code is 123456.</p>
+        <p>@otp-autofill-pi.vercel.app #123456</p>
+      </code>
+      <br />
+      <p>Enter OTP</p>
+      <OtpInput length={6} onComplete={handleOtpComplete} />
+      <p>OTP: {otp || "- - - - - -"}</p>
     </div>
   );
 }
